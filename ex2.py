1d88997552a4895ea082bb4b51705a31e8bb2a2f
Bastien Boutteau <bastien.boutteau@gmail.com>
2024-05-30 20:10:14 +0200

    Move color choice into external fx in ex2

--- ex2.py
+++ ex2.py
@@ -8,19 +8,22 @@ fen.background = "white"
 
 can = tk.Canvas(fen, bg=fen.background, height=500, width=500)
 
+def choixCouleur():
+        return str(hex(rn(0x100000, 0xffffff))).replace("0x", "#")
+
 def random_line():
     x1 = rn(0, 500)
     y1 = rn(0, 500)
     x2 = rn(0, 500)
     y2 = rn(0, 500)
-    color = str(hex(rn(0x100000, 0xffffff))).replace("0x", "#")
+    color = choixCouleur()
     can.create_line(x1, y1, x2, y2, fill=color)
 
 def random_circle():
     x = rn(0, 500)
     y = rn(0, 500)
     r = rn(0, 100)
-    color = str(hex(rn(0x100000, 0xffffff))).replace("0x", "#")
+    color = choixCouleur()
     can.create_oval(x-r, y-r, x+r, y+r, outline=color, fill=color)
 
 f1 = tk.Frame(fen, bg=fen.background)
